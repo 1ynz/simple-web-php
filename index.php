8204bdbce7d9faef61a32f6ec21335014196072f
q132645798 <q132645798@github.com>
2016-01-18 06:32:47 +0800

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title> Title </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel=stylesheet type=text/css href="css/main.css">
+	<link rel=stylesheet type=text/css href="css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row-fluid">
+			<div class="span12 well-small" align="center">
+				<h1>Title</h1>
+				<img src="image/{IMG}.jpg" alt="Image" height="300" width="300">
+			</div>
+		<div>
+		<div class="row-fluid">
+			<div class="span12 well-small" align="center">
+				<form name="form" method="post" action="login.php">
+					Username: <input type="text" name="username" /></br>
+					Password: <input type="password" name="passwd" /></br>
+					<input type="submit" class="btn btn-info" name="button" value="Login" />
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
